5530cd647420f76c3e1b695967c825a89326a980
AlexWong <alexwong@MacBook-Pro.local>
2017-09-06 11:55:38 +0800

    1

--- WebUSB/serial.js
+++ WebUSB/serial.js
@@ -81,11 +81,18 @@ var serial = {};
     }
     return result.toUpperCase();
   }
-  
+
   function arrayBufferToHexString(byteArray) {
-    return Array.from(byteArray, function(byte) {
-      return ('0' + (byte & 0xFF).toString(16)).slice(-2);
-    }).join('')
+    var s = '0x';
+    byteArray.forEach(function(byte) {
+      s += ('0' + (byte & 0xFF).toString(16)).slice(-2);
+    });
+    return s;
   }
+
+
+
+  
+
 
 })();
